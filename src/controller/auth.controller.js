12cb482243790f5c8fc957df9fc4f0e7f22504fa
maaviah17 <muawiyah1705@gmail.com>
2026-02-16 03:30:20 +0530

    added user login

--- src/controller/auth.controller.js
+++ src/controller/auth.controller.js
@@ -3,56 +3,110 @@ const jwt = require("jsonwebtoken")
 const bcrypt = require("bcrypt")
 
 async function registerUser(req,res){
+    try{
+        const {username, email , password,role} = req.body;
 
-    const {username, email , password,role} = req.body;
+        const existingUser = await userModel.findOne({
+         $or : [
+                { email },
+                { username }
+            ]
+        })
 
-    const existingUser = await userModel.findOne({
+        if(existingUser){
+            return res.status(409).json({
+                msg : "User Already Exists !!"
+            })
+        }
+
+        const hashedPassword = await bcrypt.hash(password,10);
+
+        const user = await userModel.create({
+            username,
+            email,
+            password : hashedPassword,
+            role
+        })
+
+        const token = jwt.sign(
+            {
+                id : user._id,
+                role : user.role 
+            },
+            process.env.JWT_SECRET,
+            {
+                expiresIn : '7d'
+            }
+        )
+    
+        res.cookie("token", token)
+
+        return res.status(201).json({
+            msg : "User Registered Successfully",
+            token : token,
+            user : {
+                id : user._id,
+                email : user.email,
+                username : user.username,
+                role : user.role
+            }
+        })
+    }catch (error){
+    console.error("Registration error:", error);
+    return res.status(500).json({
+      msg: "Server error during registration"
+    });
+    }
+}
+
+async function loginUser(req,res){
+
+    const {username,email,password} = req.body;
+
+    // the user will either login with (username & pass) OR (email & pass), So : 
+
+    const user = await userModel.findOne({
         $or : [
-            { email },
-            { username }
+            {email},
+            {username}
         ]
     })
 
-    if(existingUser){
-        return res.status(409).json({
-            msg : "User Already Exists !!"
+    if(!user){
+        return res.status(401).json({
+            msg : "Invalid User Credentials !!"
         })
     }
 
-    const hashedPassword = await bcrypt.hash(password,10);
+    const isPasswordValid = await bcrypt.compare(password,user.password)
 
-    const user = await userModel.create({
-        username,
-        email,
-        password : hashedPassword,
-        role
-    })
+    if(!isPasswordValid){
+        return res.status(401).json({
+            msg : "Invalid Creds"
+        })
+    }
 
-    const token = jwt.sign(
-        {
-            id : user._id,
-            role : user.role 
-        },
-        process.env.JWT_SECRET,
-        {
-            expiresIn : '7d'
-        }
-    )
-    
-    res.cookie("token", token)
+    const token = jwt.sign({
+        id : user._id,
+        role : user.role
+    }, process.env.JWT_SECRET,{
+        expiresIn : '7d'
+    });
 
-    return res.status(201).json({
-        msg : "User Registered Successfully",
-        token : token,
+    res.cookie("token",token)
+
+    res.status(200).json({
+        msg : "User Logged in successfully",
         user : {
             id : user._id,
-            email : user.email,
             username : user.username,
+            email : user.email,
             role : user.role
         }
     })
 }
 
 module.exports = {
-    registerUser
+    registerUser,
+    loginUser
 }
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const authController = require("../controller/auth.controller");
 
 router.post("/register", authController.registerUser)
+ 
+router.post("/login", authController.loginUser)
 
 module.exports = router;
