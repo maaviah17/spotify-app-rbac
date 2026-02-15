bfee22823f3fab46026657626f0cc75667cfe03b
maaviah17 <muawiyah1705@gmail.com>
2026-02-16 02:03:03 +0530

    setting up folder structure

---
+++ index.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+
+const app = require("./src/app")
+const connectDB = require("./src/db/db");
+
+const PORT = process.env.PORT;
+
+const startServer = async() => {
+
+    await connectDB();
+    app.listen(PORT,()=>{
+        console.log(`apps running at port : ${PORT} :)   `);
+    })
+}
+
+startServer();
+
---
+++ src/models/user.model.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    username : {
+        type : String,
+        required : true,
+        unique : true,
+    }, 
+    email : {
+        type : String,
+        required : true,
+        unique : true,
+    },
+    password : {
+        type : String,
+        required : true,
+        minLength : 8
+    },
+    role : {
+        type : String,
+        enum : ['user', 'artist'],
+        default : 'user'
+    }
+},{timestamps : true });
+
+const userModel = mongoose.model("user", userSchema)
+module.exports = userModel;
